92404c0b3aab5aea645b34ce416aba2c859d6c26
Takiguchii <alexandre.pupim360@gmail.com>
2025-03-28 20:26:58 -0300

    Feat Main
    Creating the first structure

---
+++ ToDoList/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace ToDoList;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ ToDoList/Responsible.cs
@@ -0,0 +1,6 @@
+namespace ToDoList;
+
+public class Responsible
+{
+    
+}
